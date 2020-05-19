4e5e97f44bc58631977a23717829230c65d88840
Jon Garman <jonegarm2@gmail.com>
2020-05-19 10:44:14 -0700

    more additions for that greenbox on gitlab lol

--- inventory_app.py
+++ inventory_app.py
@@ -5,4 +5,15 @@ food = input('Whatfood was eaten? ')
 person = input('Who ate the food? ')
 
 def menu():
-    print("")
+    print("Press 1: To add stock. ")
+    print("Press 2: To check stock. ")
+    print("Press 3: To enter purchase. ")
+    print("Press q: To add. ")
+    return input("What would you like to do? ")
+
+run = menu()
+
+while True:
+    if run == '1':
+        addStock = input('Food to be added to stock? ')
+        amount = int(input())
