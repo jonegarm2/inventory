351969c57b9b961511363f3aaae9865ad053dba3
Jon Garman <jonegarm2@gmail.com>
2020-05-22 15:48:09 -0700

    building pong paddles and ball

--- pong.py
+++ pong.py
@@ -13,6 +13,33 @@ wn.tracer(0)
 score_a = 0
 score_b = 0
 
+# paddle a
+paddle_a =turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("blue")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+
+# paddle b
+paddle_b =turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("blue")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# ball
+ball =turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("blue")
+ball.penup()
+ball.goto(0, 0)
+
 # main loop
 
 while True:
